504d0b41ca8d460e6666b0eb3166f066cc4aa18a
Gourav Mehto <grvmehto18@gmail.com>
2025-12-27 11:35:15 +0530

    First commit after project setup

---
+++ src/main/java/com/hotwax/ass/HotwaxAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hotwax.ass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotwaxAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotwaxAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotwax/ass/HotwaxAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotwax.ass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotwaxAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
